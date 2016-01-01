b4019323dfaac4c631e21bc87b6e8a87b66cd520
huyansheng <1211904437@qq.com>
2016-01-01 09:17:33 +0800

    增加将段子写入文件的功能，并且可指定写入段子的页数

--- qiushibaike.py
+++ qiushibaike.py
@@ -4,21 +4,40 @@ __author__ = 'huyansheng'
 import urllib2
 import urllib
 from bs4 import BeautifulSoup
-page =4
-url = 'http://www.qiushibaike.com/hot/page/'+str(page)
-user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
-headers = {"User-Agent":user_agent}
-try:
-    request = urllib2.Request(url,headers = headers)
-    response = urllib2.urlopen(request)
-    soup = BeautifulSoup(response.read())
-    for item in  soup.select('.content'):
-        if type(item.string) == 'bs4.elment.Comment':
-            print item.string
-        else:
-            print item
-except urllib2.URLError, e:
-    if hasattr(e,'code'):
-        print e.code
-    if hasattr(e,'reason'):
-        print e.reason
+
+
+def getStories(count,page):
+    url = 'http://www.qiushibaike.com/hot/page/'+str(page)
+    user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+    headers = {"User-Agent":user_agent}
+    result = ""
+    try:
+        request = urllib2.Request(url,headers = headers)
+        response = urllib2.urlopen(request)
+        soup = BeautifulSoup(response.read())
+        for item in  soup.select('.content'):
+            for string in item.stripped_strings:
+                result+=string
+                result+="\n"
+                print result
+                print str(page)
+            result+="\n"
+        return result
+
+    except urllib2.URLError, e:
+        if hasattr(e,'code'):
+            print e.code
+        if hasattr(e,'reason'):
+            print e.reason
+
+def main():
+    count = 0
+    all_stories = ''
+    for i in range(1,100):
+        all_stories+=getStories(count,i)
+
+    output = file("./qiushibaike.txt","w")
+    output.writelines(all_stories.encode("utf-8"))
+    output.close()
+if "__main__" == main():
+    main()
